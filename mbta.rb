ad8286ca46e300fb4dde7aa112c0dbcd58711355
micfin <michael.finneran@generalassemb.ly>
2019-02-24 14:36:00 +0300

    add hw

---
+++ mbta.rb
@@ -0,0 +1,13 @@
+# Your solution here
+class Subway
+    def stops_between_stations(start_line, start_station, end_line, end_station)
+    end
+end
+  
+# One line, all the stations on that line
+class Line
+end
+
+# One station
+class Station
+end
