c9e4b9eecba108b67d0e505e8c52ca5c11ccc500
Mohammed <moh.jameel178@gmail.com>
2019-03-02 20:53:54 +0300

    Add some of my soluton that i did , and for the rest i wrote pesdo code for my idea to solove this problem

--- mbta.rb
+++ mbta.rb
@@ -1,13 +1,39 @@
 # Your solution here
+$lines = {
+    'Red' => ['South Station', 'Park Street','Kendall','Central','Harvard','Harvard','Davis','Alewife'],
+    'Greeen' => ['Government Center','Park Street','Boylston','Arlington','Copley','Hynes', 'Kenmore'],
+    'Orange' => ['North Station', 'Haymarket','Park Street','State','Downtown Crossing','Chinatown','Back Bay','Forest Hills']
+}
 class Subway
-    def stops_between_stations(start_line, start_station, end_line, end_station)
+    
+    def initialize(lineRed, lineGreen, lineOrange)
+        @lineRed = lineRed
+        @lineGreen = lineGreen
+        @lineOrange = lineOrange
     end
-end
   
-# One line, all the stations on that line
-class Line
-end
+    def stops_between_stations(start_line, start_station, end_line, end_station)
+        # create if def to count the stops between stations and lines if the start line is the same for 
+    #  line 
+    #  also we need another if conditions for diffrent lines , 
 
-# One station
-class Station
-end
+    end
+  end
+  
+  class Line
+    attr_reader :stations
+    attr_reader :name
+
+    # for the same line to count the stations , we can use (-) between the stations ,
+    #  and show for the user the stations that will use it
+  
+    def initialize(name, stations)
+    end
+  end
+  
+  class Station
+    attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+  end
