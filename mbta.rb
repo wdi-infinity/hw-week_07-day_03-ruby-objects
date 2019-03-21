50d45a494be7b86d7fe2fca17e03d1deca5f2300
hessaAqeel <hessa1aqeel@gmail.com>
2019-03-21 21:33:56 +0300

    HW

--- mbta.rb
+++ mbta.rb
@@ -1,13 +1,27 @@
-# Your solution here
-class Subway
+lines = {
+red_line = ['South Station','Park Street','Kendall','Central','Harvard','Porter','Davis','Alewife'],
+green_line = ['Government Center','Park Street','Boylston','Arlington','Copley','Hynes','Kenmore'],
+orange_line = ['North Station','Haymarket','Park Street','State','Downtown Crossing','Chinatown''Back Bay','Forest Hills' ] 
+}
+
+
+  class Subway
     def stops_between_stations(start_line, start_station, end_line, end_station)
+        if (start_line == end_line )
+            return start_station - end_station
+        else return  
+        end 
     end
 end
-  
-# One line, all the stations on that line
+
 class Line
 end
 
-# One station
 class Station
 end
+
+
+@mbta = Subway.new
+@mbta.stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
+@mbta.stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
+@mbta.stops_between_stations('Red', 'South Station', 'Green', 'Kenmore')
